1b2f4cd81181ba7d2471bf0d62c3da4bf15c25f6
showaya Master <precious.stamp.line@gmail.com>
2025-06-01 01:54:45 +0900

    コメント

--- code/routes/lineWebhook.js
+++ code/routes/lineWebhook.js
@@ -1,3 +1,23 @@
+// code/routes/lineWebhook.js の最初に追加して、一時的に return
+module.exports = (lineClient) => (req, res) => {
+  const event = req.body.events[0];
+  if (event?.replyToken) {
+    // ★テスト用固定返信
+    lineClient.replyMessage(event.replyToken, {
+      type: 'text',
+      text: 'テスト返信：サーバーまでは届きました！'
+    })
+      .then(() => res.status(200).end())
+      .catch(err => {
+        console.error('LINE reply error:', err);
+        res.status(500).end();
+      });
+    return;          // ← openaiClient へは進まない
+  }
+  res.status(200).end();
+};
+
+
 // lineWebhook.js ＝ LINE から届いたイベントを OpenAI へ橋渡しするルータ
 const handleEvent = require('../services/openaiClient');
 
